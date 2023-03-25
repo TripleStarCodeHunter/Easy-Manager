0039911457568cdd0a9b476acff895a693bf12d1
vinayaka-iyer <vinayakaiyer999@gmail.com>
2023-03-25 16:39:07 +0530

    Initialized Backend and Frontend

---
+++ frontend/src/config/serverApiConfig.js
@@ -0,0 +1,12 @@
+console.log(
+  "process env REACT_APP_DEV_REMOTE",
+  process.env.REACT_APP_DEV_REMOTE
+);
+
+export const API_BASE_URL =
+  // process.env.NODE_ENV == "production" ||
+  // process.env.REACT_APP_DEV_REMOTE ==
+  "http://localhost:8888/api/";
+
+// export const API_BASE_URL = "https://wild-puce-reindeer-sari.cyclic.app/api/";
+export const ACCESS_TOKEN_NAME = "x-auth-token";
